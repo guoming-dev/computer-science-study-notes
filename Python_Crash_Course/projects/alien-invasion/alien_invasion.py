aa51d29e9a4087ae75b438fc11bcbdf2e1cc210f
guoming-dev <gm_learning_tech@outlook.com>
2025-03-17 18:12:57 +0800

    Finished coding for piloting the ship left and right.

--- Python_Crash_Course/projects/alien-invasion/alien_invasion.py
+++ Python_Crash_Course/projects/alien-invasion/alien_invasion.py
@@ -24,7 +24,7 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-
+            self.ship.update()
             # Redraw the screen during each pass through the loop.
             self._update_screen()
             self.clock.tick(60)
@@ -34,6 +34,20 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left.
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    # Stop the ship from moving right.
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    # Stop the ship from moving left.
+                    self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
