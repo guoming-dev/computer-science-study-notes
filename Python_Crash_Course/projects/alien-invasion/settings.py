d27a9a0f84433c284db7bcd6ea8eb7072d899d3d
guoming-dev <gm_learning_tech@outlook.com>
2025-03-21 00:19:26 +0800

    Finished Chapter 12 of Python Crash Course and successfully implemented the bullet stage.

--- Python_Crash_Course/projects/alien-invasion/settings.py
+++ Python_Crash_Course/projects/alien-invasion/settings.py
@@ -6,7 +6,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (0, 0, 255)
+        self.bg_color = (233, 233, 233)
 
         # Ship settings
         self.ship_speed = 1.5
@@ -16,3 +16,4 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
