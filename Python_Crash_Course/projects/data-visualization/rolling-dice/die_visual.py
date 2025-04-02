a7d3ee0a31baec590970852b85d9cb539510d2ae
guoming-dev <gm_learning_tech@outlook.com>
2025-04-02 21:56:33 +0800

    Completed Chapter 15 exercise and rolling dice project.

--- Python_Crash_Course/projects/data-visualization/rolling-dice/die_visual.py
+++ Python_Crash_Course/projects/data-visualization/rolling-dice/die_visual.py
@@ -2,24 +2,30 @@ import plotly.express as px
 
 from die import Die
 
-# Create a D6.
-die = Die()
+# Create a D6 and a D10.
+die_1 = Die(6)
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list.
 results = []
-for _ in range(1000):
-    result = die.roll()
+for _ in range(50_000):
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the results.
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result+1)
 for value in poss_results:
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-fig = px.bar(x=poss_results, y=frequencies)
-fig.show()
+title = "Results of Rolling a D6 and a D10 50,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
 
-print(frequencies)
+# Further customize chart.
+fig.update_layout(xaxis_dtick=1)
+
+fig.write_html('dice_visual_d6d10.html')
