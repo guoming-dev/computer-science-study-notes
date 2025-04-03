39175ccb2b1202c212996cec82b59707ab445a0f
guoming-dev <gm_learning_tech@outlook.com>
2025-04-03 10:54:46 +0800

    Plotting a Longer Timeframe

--- Python_Crash_Course/projects/data-visualization/weather_data/sitka_highs.py
+++ Python_Crash_Course/projects/data-visualization/weather_data/sitka_highs.py
@@ -3,7 +3,7 @@ import csv
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-path = Path('./sitka_weather_07-2021_simple.csv')
+path = Path('./sitka_weather_2021_simple.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 
 reader = csv.reader(lines)
@@ -23,7 +23,7 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red')
 
 # Format plot.
-ax.set_title("Daily High Temperatures, July 2021", fontsize=24)
+ax.set_title("Daily High Temperatures, 2021", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
