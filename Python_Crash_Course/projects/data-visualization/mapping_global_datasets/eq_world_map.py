a87a71d916d0a3dedc761b60c044ab2f02625d24
guoming-dev <gm_learning_tech@outlook.com>
2025-04-04 21:18:13 +0800

    Refactor the codes

--- Python_Crash_Course/projects/data-visualization/mapping_global_datasets/eq_world_map.py
+++ Python_Crash_Course/projects/data-visualization/mapping_global_datasets/eq_world_map.py
@@ -11,21 +11,20 @@ all_eq_data = json.loads(contents)
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, eq_titles = [], [], [], []
 for eq_dict in all_eq_dicts:
-    mag = eq_dict['properties']['mag']
-    lon = eq_dict['geometry']['coordinates'][0]
-    lat = eq_dict['geometry']['coordinates'][1]
-    mags.append(mag)
-    lons.append(lon)
-    lats.append(lat)
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    eq_titles.append(eq_dict['properties']['title'])
 
-title = 'Global Earthquakes'
+title = all_eq_data['metadata']['title']
 fig = px.scatter_geo(
     lat=lats, lon=lons, size=mags, title=title,
     color=mags,
     color_continuous_scale='plasma',
     labels={'color': 'Magnitude'},
     projection='natural earth',
+    hover_name=eq_titles,
 )
 fig.show()
