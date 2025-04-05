9ade4dd5b5b9e8e5263899053c008770a87f3a28
guoming-dev <gm_learning_tech@outlook.com>
2025-04-06 00:06:52 +0800

    Customizing Marker Colors

--- Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos_visual.py
+++ Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos_visual.py
@@ -18,9 +18,7 @@ repo_dicts = response_dict['items']
 repo_links, stars, hovered_texts = [], [], []
 for repo_dict in repo_dicts:
     # Turn repo names into active links.
-    repo_name = repo_dict['name']
-    repo_url = repo_dict['html_url']
-    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_link = f"<a href='{repo_dict['html_url']}'>{repo_dict['name']}</a>"
     repo_links.append(repo_link)
     stars.append(repo_dict['stargazers_count'])
 
@@ -34,4 +32,7 @@ labels = {'x': 'Repository', 'y': 'Stars'}
 fig = px.bar(x=repo_links, y=stars, title=title, labels=labels, hover_name=hovered_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
+
+fig.update_traces(marker_color='SteelBlue', marker_opacity=0.6)
+
 fig.show()
