889b6f81d4d37cc5ed3f4184c481c6bfcac90134
guoming-dev <gm_learning_tech@outlook.com>
2025-04-05 23:41:53 +0800

    Adding Custom Tooltips

--- Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos_visual.py
+++ Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos_visual.py
@@ -15,15 +15,19 @@ print(f"Complete Results: {not response_dict['incomplete_results']}")
 
 # Process repository information.
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, hovered_texts = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
 
+    # Build hover texts.
+    hovered_text = f"{repo_dict['owner']['login']}<br />{repo_dict['description']}"
+    hovered_texts.append(hovered_text)
+
 # Make visualization.
 title = "Most-Starred Python Projects on GitHub"
 labels = {'x': 'Repository', 'y': 'Stars'}
-fig = px.bar(x=repo_names, y=stars, title=title, labels=labels)
+fig = px.bar(x=repo_names, y=stars, title=title, labels=labels, hover_name=hovered_texts)
 
 fig.update_layout(title_font_size=28, xaxis_title_font_size=20, yaxis_title_font_size=20)
 fig.show()
