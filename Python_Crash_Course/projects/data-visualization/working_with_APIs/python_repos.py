887d285129faa0908fac3b8c8f29ddd0660b27de
guoming-dev <gm_learning_tech@outlook.com>
2025-04-05 13:16:06 +0800

    Visualizing Repositories Using Plotly

--- Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos.py
+++ Python_Crash_Course/projects/data-visualization/working_with_APIs/python_repos.py
@@ -10,6 +10,22 @@ print(f"Status code: {r.status_code}")
 
 # Convert the response object to a dictionary
 response_dict = r.json()
+print(f"Total repositories: {response_dict['total_count']}")
+print(f"Complete Results: {not response_dict['incomplete_results']}")
 
-# Process results.
-print(response_dict.keys())
+# Explore information about the repositories.
+repo_dicts = response_dict['items']
+print(f"Repositories returned: {len(repo_dicts)}")
+
+# Examine the first repository
+repo_dict = repo_dicts[0]
+
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
